6abf4f3d44a6fd34a8290d92cf275de751dbd580
Enidbii <chebsenid@gmail.com>
2023-04-26 22:11:51 +0000

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -28,12 +28,13 @@ void selection_sort(int *array, size_t size)
 	{
 		min_idx = i;
 		for (j = i + 1; j < size; j++)
+		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
-
+		}
 		if (min_idx != i)
 		{
-			swap(&array, array[min_idx], array[i]);
+			swap(&array, min_idx, i);
 			print_array(array, size);
 		}
 	}
